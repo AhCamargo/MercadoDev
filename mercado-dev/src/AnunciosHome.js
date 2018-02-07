d94f8bbe53ec893b81b94f1c2048c08c746264e9
acamargo2017 <andre.camargo500@gmail.com>
2018-02-07 13:59:27 -0200

    upload de fotos

--- mercado-dev/src/AnunciosHome.js
+++ mercado-dev/src/AnunciosHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const AnunciosHome = ({anuncio}) => {
     return (
         <div className="col-lg-4 col-md-6 mb-4">
--- mercado-dev/src/base.js
+++ mercado-dev/src/base.js
@@ -3,15 +3,18 @@ const config = {
     authDomain: "mercado-dev-adb83.firebaseapp.com",
     databaseURL: "https://mercado-dev-adb83.firebaseio.com",
     projectId: "mercado-dev-adb83",
-    storageBucket: "",
+    storageBucket: "gs://mercado-dev-adb83.appspot.com",
     messagingSenderId: "188067147434"
   }
 
   const Rebase = require('re-base')
   const firebase = require('firebase/app')
   require('firebase/database')
+  require('firebase/storage')
 
   const app = firebase.initializeApp(config)
   const base = Rebase.createClass(app.database())
+
+  export const storage = app.storage()
 
   export default base
